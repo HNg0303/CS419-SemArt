9b8e058596eec1d7415da3244b6ab48edf57c840
LeeHoang2710 <builehoang.qn@gmail.com>
2025-12-22 11:00:50 +0700

    Add test images for demo

--- front-end/app/page.tsx
+++ front-end/app/page.tsx
@@ -148,6 +148,7 @@ export default function App() {
 
   const clearUploadedImage = () => {
     setUploadedImage(null);
+    setIsSearching(false);
     setInputQuery("");
     setSubmittedQuery("");
     setSearchResults(artDatabase);
